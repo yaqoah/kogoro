4965226e8344a83a4bb2e071b411e709cd11a087
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-10-17 12:35:27 +0100

    first commit

---
+++ bot/utils/commands.py
@@ -0,0 +1,11 @@
+class _Bot_commands:
+    def __init__(self):
+        self.CONTACT_COMMAND = "contact"
+        self.START_COMMAND = "start"
+        self.PREDICT_COMMAND = "predict"
+        self.LEAGUE_STATISTICS_COMMAND = "league"
+        self.TEAM_STATISTICS_COMMAND = "team"
+        self.PLAYER_STATISTICS_COMMAND = "player"
+
+
+mybots = _Bot_commands()
